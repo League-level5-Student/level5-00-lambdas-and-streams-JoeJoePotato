3abaf2bc84edb3a909b31f3b77f35a21c376af50
JoeJoePotato <34780073+JoeJoePotato@users.noreply.github.com>
2020-10-21 18:14:50 -0700

    Lambamethods

--- src/_01_Lambda_Methods/LambdaMethods.java
+++ src/_01_Lambda_Methods/LambdaMethods.java
@@ -15,20 +15,57 @@ public class LambdaMethods {
 		//2. Call the printCustonMessage method using a lambda so that the String prints backwards.
 		printCustomMessage((s)->{
 			String z=new String();
+			String bleh=new String();
 			for(int j=0; j<s.length(); j++) {
-				z.charAt(j)=s.charAt(s.length()-j);
+				 bleh=s.substring(s.length()-j-1, s.length()-j);
+				z=z+bleh;
 			}
+			System.out.println(z);
 		}, "hello my good rat star");
 		//3. Call the printCustonMessage method using a lambda so that the String prints with a mix between upper an lower case characters.
-		
+		printCustomMessage((s)->{
+			String z=new String();
+		char bleh;
+			for(int j=0; j<s.length(); j++) {
+				 bleh=s.charAt(j);
+				 if(j%3==2 || j%4==3) {
+					
+				 }
+				z=z+bleh;
+			}
+			System.out.println(z);
+		}, "hello my good rat star");
 		//4. Call the printCustonMessage method using a lambda so that the String prints with a period in between each character.
-		
+		printCustomMessage((s)->{
+			String z=new String();
+			String bleh=new String();
+			for(int j=0; j<s.length(); j++) {
+				 bleh=s.substring(s.length()-j-1, s.length()-j);
+				if(j>0) {
+				 z=z+".";
+				}
+				 z=z+bleh;
+			}
+			System.out.println(z);
+		}, "hello my good rat star");
 		//5. Call the printCustonMessage method using a lambda so that the String prints without any vowels.
-	
+		printCustomMessage((s)->{
+			String z=new String();
+			char bleh;
+			for(int j=0; j<s.length(); j++) {
+				if(s.charAt(j)!='a' && s.charAt(j)!='e' && s.charAt(j)!='i' && s.charAt(j)!='o' && s.charAt(j)!='u') {
+				bleh=s.charAt(j);
+				z=z+bleh;
+				}
+				
+			}
+			System.out.println(z);
+		}, "hello my good rat star");
 	}
 	
 	public static void printCustomMessage(SpecialPrinter sp, String value) {
 		sp.printSpecial(value);
+		
 	}
 }
 	
